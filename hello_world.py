8a008489d4eba0ac537483b9fd3a7cdd1f0aaeac
xiaowang <132003197+yiwang-maker@users.noreply.github.com>
2024-12-28 23:39:19 +0800

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+print("Hello Python world!")
+ab="What is it?"
+print(ab)
+
+simple_message="我一定要积极向上！"
+print(simple_message)
+simple_message="我必须顽强挣扎！"
+print(simple_message)
+
+name = "jayden"
+name = name.title()
+a = "Hello"
+b = "would you like to learn some Python today?"
+sentence = f"{a} {name}, {b}"
+print(sentence)
+
+name = "jayden miller"
+name = name.lower()
+print(name)
+name = name.upper()
+print(name)
+name = name.title()
+print(name)
+
+name = "albert einstein"
+famous_person = name.title()
+saying = '"A person who never made a mistake never tried anything new."'
+message = f"{famous_person} once said, {saying}"
+print(message)
+
+name = "   albert einstein  "
